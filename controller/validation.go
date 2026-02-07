64186f44a635dd4c15c9f693f395a9d0e4e7eb39
iwariku <02rikuto18@gmail.com>
2026-02-07 10:52:36 +0900

    refactor: signup password validation

--- controller/validation.go
+++ controller/validation.go
@@ -2,45 +2,20 @@ package controller
 
 import (
 	"errors"
-	"strings"
-	"unicode"
+	"regexp"
 )
 
 func validatePassword(p string) error {
 	if len(p) < 8 {
 		return errors.New("パスワードは8文字以上にしてください")
 	}
-
-	var (
-		hasUpper  bool
-		hasLower  bool
-		hasNumber bool
-		hasSymbol bool
-	)
-
-	for _, char := range p {
-		switch {
-		case unicode.IsUpper(char):
-			hasUpper = true
-		case unicode.IsLower(char):
-			hasLower = true
-		case unicode.IsDigit(char):
-			hasNumber = true
-		case strings.ContainsRune("!?-_", char):
-			hasSymbol = true
-		}
-	}
-
-	if !hasUpper {
+	if !regexp.MustCompile(`[A-Z]`).MatchString(p) {
 		return errors.New("大文字を含めてください")
 	}
-	if !hasLower {
-		return errors.New("小文字を含めてください")
-	}
-	if !hasNumber {
+	if !regexp.MustCompile(`[0-9]`).MatchString(p) {
 		return errors.New("数字を含めてください")
 	}
-	if !hasSymbol {
+	if !regexp.MustCompile(`[!?_\-]`).MatchString(p) {
 		return errors.New("記号(!?-_)を1文字以上含めてください")
 	}
 
