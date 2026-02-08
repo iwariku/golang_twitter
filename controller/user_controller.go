0e562a4709667bfd3a82dc03681657eb4d959027
iwariku <02rikuto18@gmail.com>
2026-02-08 15:18:57 +0900

    refactor: activation_controllerのアクティベーションロジックをuser_controllerに移動

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -21,6 +21,7 @@ import (
 
 type UserController struct {
 	Queries *db.Queries
+	Mailer  auth.MailerInterface
 }
 
 // SignUpの流れ
@@ -72,7 +73,7 @@ func (uc *UserController) SignUp(c *gin.Context) {
 		return
 	}
 
-	err = auth.SendActivationEmail(user.Mail, token)
+	err = uc.Mailer.SendActivationEmail(user.Mail, token)
 	if err != nil {
 		log.Printf("メールの送信に失敗しました: %v", err)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "メールの送信に失敗しました"})
@@ -80,4 +81,21 @@ func (uc *UserController) SignUp(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusCreated, user)
+}
+
+func (uc *UserController) Activate(c *gin.Context) {
+	// URLから"token"という名前のパラメータを取得する
+	token := c.Query("token")
+	if token == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "トークンが必要です"})
+		return
+	}
+
+	err := uc.Queries.ActivateUser(c.Request.Context(), pgtype.Text{String: token, Valid: true})
+	if err != nil {
+		log.Printf("有効化エラー: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "有効化に失敗しました"})
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "アカウントが有効になりました。ログインが可能な状態です。"})
 }
