c15376fb1e08a10df0f599a39729f661c96a6653
iwariku <02rikuto18@gmail.com>
2026-03-01 23:05:48 +0900

    feat: retweetsテーブルとリツイートクエリを定義

--- db/query.sql
+++ db/query.sql
@@ -112,3 +112,99 @@ SELECT
 FROM tweets t
 ORDER BY t.created_at DESC
 LIMIT $2 OFFSET $3;
+
+-- リツイート機能
+-- ツイート詳細、いいね、リツイート付き(リツイートができたら巻き替え)
+-- name: GetTweetsWithLikeAndRetweet :one
+SELECT
+  t.id,
+  t.user_id,
+  t.content,
+  t.created_at,
+  (SELECT COUNT(*) FROM likes l WHERE l.tweet_id = t.id) AS like_count,
+  EXISTS (
+    SELECT 1
+    FROM likes l
+    WHERE l.tweet_id = t.id AND l.user_id = $1
+  ) AS is_liked,
+  (SELECT COUNT(*) FROM retweets r WHERE r.tweet_id = t.id ) AS retweet_count,
+  EXISTS (
+    SELECT 1
+    FROM retweets r
+    WHERE r.tweet_id = t.id AND r.user_id = $1
+  ) AS is_retweeted
+FROM tweets t
+WHERE t.id = $2;
+
+-- ツイート一覧、いいね、リツイート付き(リツイートができたら巻き替え)
+-- name: GetTweetsWithRetweet :many
+SELECT
+  t.id,
+  t.user_id,
+  t.content,
+  t.created_at,
+    (SELECT COUNT(*) FROM likes l WHERE l.tweet_id = t.id) AS like_count,
+  EXISTS (
+    SELECT 1
+    FROM likes l
+    WHERE l.tweet_id = t.id AND l.user_id = $1
+  ) AS is_liked,
+  (SELECT COUNT(*) FROM retweets r WHERE r.tweet_id = t.id) AS retweet_count,
+  EXISTS (
+    SELECT 1
+    FROM retweets r
+    WHERE r.tweet_id = t.id AND r.user_id = $1
+  ) AS is_retweeted
+FROM tweets t
+ORDER BY t.created_at DESC
+LIMIT $2 OFFSET $3;
+
+-- ユーザー詳細でのツイート一覧、いいね、リツイート付き(リツイートができたら巻き替え)
+-- name: GetTweetsByUserIDWithRetweet :many
+SELECT
+  t.id,
+  t.user_id,
+  t.content,
+  t.created_at,
+  (SELECT COUNT(*) FROM likes l WHERE l.tweet_id = t.id) AS like_count,
+  EXISTS (
+    SELECT 1
+    FROM likes l
+    WHERE l.tweet_id = t.id AND l.user_id = @viewer_user_id::int
+  ) AS is_liked,
+  (SELECT COUNT(*) FROM retweets r WHERE r.tweet_id = t.id) AS retweet_count,
+  EXISTS (
+    SELECT 1
+    FROM retweets r
+    WHERE r.tweet_id = t.id AND r.user_id = @viewer_user_id::int
+  ) AS is_retweeted
+FROM tweets t
+WHERE t.user_id = @target_user_id::int
+ORDER BY t.created_at DESC
+LIMIT @limit_val::int OFFSET @offset_val::int;
+
+-- 選択したユーザーがリツイートしているツイート一覧
+-- name: GetRetweetedTweetsByUserID :many
+SELECT
+  t.id,
+  t.user_id,
+  t.content,
+  user_retweets.created_at,
+  (SELECT COUNT(*) FROM likes l WHERE l.tweet_id = t.id) AS like_count,
+  EXISTS (
+    SELECT 1
+    FROM likes l
+    WHERE l.tweet_id = t.id AND l.user_id = $1
+  ) AS is_liked,
+
+  (SELECT COUNT(*) FROM retweets viewer_retweet WHERE viewer_retweet.tweet_id = t.id ) AS retweet_count,
+  EXISTS (
+    SELECT 1
+    FROM retweets viewer_retweet
+    WHERE viewer_retweet.tweet_id = t.id AND viewer_retweet.user_id = $1
+  ) AS is_retweeted
+FROM retweets user_retweets
+JOIN tweets t ON user_retweets.tweet_id = t.id
+WHERE user_retweets.user_id = $2
+ORDER BY user_retweets.created_at DESC
+LIMIT $3 OFFSET $4;
