820e20e59050dab30b7d438cff04919f286a6dc4
iwariku <02rikuto18@gmail.com>
2026-03-12 18:05:18 +0900

    feat: フォローボタンアクションの実装

--- main.go
+++ main.go
@@ -86,8 +86,9 @@ func main() {
 
 		authGroup.POST("/api/tweets/:id/bookmark", tc.CreateBookmark)
 		authGroup.DELETE("/api/tweets/:id/bookmark", tc.DeleteBookmark)
-		authGroup.POST("/api/follow-users/:id/follow", uc.CreateFollow)
-		authGroup.DELETE("/api/follow-users/:id/follow", uc.DeleteFollow)
+
+		authGroup.POST("/api/users/:id/follow", uc.CreateFollow)
+		authGroup.DELETE("/api/users/:id/follow", uc.DeleteFollow)
 
 		authGroup.GET("/api/users/:id/followings", uc.GetFollowings)
 		authGroup.GET("/api/users/:id/followers", uc.GetFollowers)
