c76a3b7e9b9074560d8a5d2fd380e5ea43821e17
iwariku <02rikuto18@gmail.com>
2026-05-01 18:14:45 +0900

    fix: if/else文の構文エラーの解消

--- db/connect.go
+++ db/connect.go
@@ -12,34 +12,28 @@ import (
 func ConnectDB(ctx context.Context) (*pgx.Conn, *Queries) {
 	databaseUrl := os.Getenv("DATABASE_URL")
 
-	if databaseUrl == "" {
+	var connStr string
 
+	if databaseUrl == "" {
+		// ローカル（Docker）
 		user := os.Getenv("POSTGRES_USER")
 		pass := os.Getenv("POSTGRES_PASSWORD")
 		host := "postgres"
 		port := "5432"
 		dbname := os.Getenv("POSTGRES_DB")
 
-		connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", user, pass, host, port, dbname)
-
-		conn, err := pgx.Connect(ctx, connStr)
-		if err != nil {
-			log.Fatalf("DBに接続できませんでした: %v", err)
-		}
-
-		fmt.Println("DB接続に成功しました")
-	}
-
+		connStr = fmt.Sprintf("postgres://%s:%s@%s:%s/%s", user, pass, host, port, dbname)
 	} else {
-
-
-			conn, err := pgx.Connect(ctx, connStr)
-		if err != nil {
-			log.Fatalf("DBに接続できませんでした: %v", err)
-		}
-
-		fmt.Println("DB接続に成功しました")
-
+		// Render（本番）
+		connStr = databaseUrl
 	}
-conn, err := pgx.Connect(ctx, connStr)
+
+	conn, err := pgx.Connect(ctx, connStr)
+	if err != nil {
+		log.Fatalf("DBに接続できませんでした: %v", err)
+	}
+
+	fmt.Println("DB接続に成功しました")
+
+	return conn, New(conn)
 }
