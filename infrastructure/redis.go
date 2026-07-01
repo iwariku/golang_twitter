24f6a30e2fb419f9be519cf81f348cb8c78853ee
iwariku <02rikuto18@gmail.com>
2026-07-01 23:29:24 +0900

    feat: AWS Elastic CacheのためのTLS設定を追加

--- infrastructure/redis.go
+++ infrastructure/redis.go
@@ -2,6 +2,8 @@ package infrastructure
 
 import (
 	"context"
+	"crypto/tls"
+	"strings"
 
 	"github.com/iwariku/golang_twitter/utils"
 
@@ -11,10 +13,19 @@ import (
 func NewRedisClient() *redis.Client {
 	redisAddress := utils.GetEnvOrDefault("REDIS_ADDRESS", "redis_server:6379")
 
+	// アドレスをベースに、AWS環境（TLS必須）かどうかの判定を行う
+	var tlsConfig *tls.Config
+	if strings.Contains(redisAddress, "amazonaws.com") {
+		tlsConfig = &tls.Config{
+			InsecureSkipVerify: true,
+		}
+	}
+
 	redisClient := redis.NewClient(&redis.Options{
-		Addr:     redisAddress,
-		Password: "",
-		DB:       0,
+		Addr:      redisAddress,
+		Password:  "",
+		DB:        0,
+		TLSConfig: tlsConfig,
 	})
 
 	ctx := context.Background()
